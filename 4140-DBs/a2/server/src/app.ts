6ff8b6b82ca9b1a18a013aef6840b51f613464eb
parth <parthpatel@TE458.WPA.Dal.Ca>
2022-10-06 15:07:07 -0300

    Remove prisma as its not stable

--- 4140-DBs/a2/server/src/app.ts
+++ 4140-DBs/a2/server/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
+import supabase from "./lib/supabase-client";
 
+// Loads .env
 console.log(dotenv.config());
 
 const PORT = process.env.PORT;
@@ -12,6 +14,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/status", (req: Request, res: Response) => {
   res.status(200).json({ app: "a2-server", status: "OK" });
+});
+
+app.get("/parts", async (req: Request, res: Response) => {
+  try {
+    const { data, error } = await supabase.from("part471").select();
+    console.log(data);
+    res.status(200).json({ test: "test" });
+  } catch (error) {
+    const error2 = error as Error;
+    console.log(error);
+    console.log(error2.message);
+  }
 });
 
 app.listen(PORT, () => {
