eb13686efb7a774424455fa4d024978ab3572900
parth <parthpatel@TE458.WPA.Dal.Ca>
2022-10-06 14:20:47 -0300

    Add prisma, generate db schema in prisma and generate prisma client

--- 4140-DBs/a2/server/src/app.ts
+++ 4140-DBs/a2/server/src/app.ts
@@ -1,11 +1,18 @@
 import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
 
-const PORT = 8080;
+console.log(dotenv.config());
+
+const PORT = process.env.PORT;
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/status", (req: Request, res: Response) => {
+  res.status(200).json({ app: "a2-server", status: "OK" });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
